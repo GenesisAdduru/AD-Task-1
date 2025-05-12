ef559652c2598e98f4208f9ca9888f1d02d416bd
GenesisAdduru <202310334@fit.edu.ph>
2025-05-12 23:57:49 +0800

    feat(travel page): Added heading

--- page/index.php
+++ page/index.php
@@ -4,3 +4,6 @@
     <title>Travel Journal</title>
     <link rel="stylesheet" href="/page/assets/css/example.css"> <!-- External CSS file -->
 </head>
+<body>
+<h1>✈️ WeatherWanderer Travel Journal</h1>
+</html>
