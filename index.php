b436302488a3dd55e633bbb2ddfede9d32562bcf
GenesisAdduru <202310334@fit.edu.ph>
2025-05-12 23:38:35 +0800

    feat (landing page): Added HTML Structure and Connected CSS

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>WeatherWanderer - Travel Forecast</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<h1>🌍 WeatherWanderer Dashboard</h1>
+
+
+</body>
+</html>
